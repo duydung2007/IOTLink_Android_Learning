5f538bcbc59b9f732df0cc1431bd7d732e777ca2
Duy Dung <duydung2007@gmail.com>
2020-09-06 16:43:21 +0700

    + create project Sunset

---
+++ Sunset/app/src/main/java/android/bignerdranch/sunset/SunsetActivity.kt
@@ -0,0 +1,12 @@
+package android.bignerdranch.sunset
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SunsetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
